c7f57aede43fe152f8352ce3544b94d9a71b042f
Praveena Prasannan <49147219+PraveenaPrasannan4789@users.noreply.github.com>
2026-02-27 13:57:24 +0530

    added array method find examples

---
+++ 03-array-methods/methods/find.js
@@ -0,0 +1,12 @@
+//examples
+
+//find first number >10
+let numbers=[50,90,70,67];
+let result= numbers.find((num)=>{
+   return num %2 ==0
+
+})
+console.log('number',result)
+
+//finding the user 
+//no match scenario
