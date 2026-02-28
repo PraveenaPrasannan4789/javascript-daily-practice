33dda8e9199045632f281f9736c377d37698874b
Praveena Prasannan <49147219+PraveenaPrasannan4789@users.noreply.github.com>
2026-02-28 21:19:08 +0530

    added array method some every examples

--- 03-array-methods/methods/find.js
+++ 03-array-methods/methods/find.js
@@ -8,5 +8,25 @@ let result= numbers.find((num)=>{
 })
 console.log('number',result)
 
+
+
 //finding the user 
+let users=[
+    {
+        name:"ammu",
+        age:23
+    },
+    {
+        name:"veena",
+        age:35
+    }
+]
+let eligible = users.find((x)=>x.age<25)
+console.log('eligible',eligible)
+
+
+
 //no match scenario
+let matches=['mumbai','agra'];
+let res= matches.find((x)=>x.length>9)
+console.log('result',res)
---
+++ 03-array-methods/methods/some.js
@@ -0,0 +1,12 @@
+//examples
+
+//check odd number exist
+
+const oddNumbers=[3,7,8]
+const check= oddNumbers.some((x)=>{
+    return x%3 === 0;
+})
+const checkEvery=oddNumbers.every((x)=>{
+    return x%3 ===0;
+})
+console.log('odd Number Exists',check,checkEvery)
