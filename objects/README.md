fef832afd8664a61a198cdaff11f1226899e15f7
Praveena Prasannan <49147219+PraveenaPrasannan4789@users.noreply.github.com>
2026-03-05 12:16:45 +0530

    object basics added

---
+++ objects/README.md
@@ -0,0 +1,11 @@
+# Day 4 
+# Objects & Destructuring
+
+Topics Covered:
+- Object creation
+- Property access
+- Nested objects
+- Object methods
+- Destructuring
+- Spread operator
+- Practice problems
--- objects/basics.js
+++ objects/basics.js
@@ -1,3 +1,4 @@
+//Example 1:
 // Creating object
 const user = {
   name: "Praveena",
@@ -13,3 +14,34 @@ user.city = "London";
 
 // Deleting property
 delete user.age;
+
+
+//Example 2
+
+const employees={
+    name:"ABc",
+    department:"sales",
+    salary:45000
+}
+
+//dot notation
+console.log(employees.name);
+console.log(employees.department);
+console.log(employees.salary);
+employees.name='xyz';
+
+
+//bracket notation
+console.log(employees["name"]); //used when property name is dynamic,property comes from a variable
+employees["name"]="abc";
+console.log(employees["name"]);
+
+
+//adding,updating,deleting
+employees.age= 34;
+employees["age"]=55;
+console.log(employees);
+delete employees.age;
+console.log(employees);
+
+
